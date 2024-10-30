88cf1b986929e56e0e3648f3b3c1bbf02aceed57
DevZenzya34 <devzenzya34@gmail.com>
2024-10-30 11:22:23 +0100

    Add /videos route to view stream

--- video-stream/Readme.md
+++ video-stream/Readme.md
@@ -1,5 +1,8 @@
 # The first microservice video stream app
 Source: Bootstrapping Microservices, Second Edition by Ashley Davis
 
-1 - Create a nodejs project with `npm init -y` and customize package.json
-2 - Install Express for HTTP server
+1- Create a nodejs project: `npm init -y` and customize package.json
+    
+2- Install Express for HTTP server and launch: `pnpm start` or use nodemon to manage server
+
+3- Add the streaming video
--- video-stream/express.js
+++ video-stream/express.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import fs from 'fs';
 
 
 const app = express();
 const port = 3000;
 
+// Index route
 app.get('/', (req, res) => {
     res.send('Videos Streaming App Server');
+})
+
+// Http stream route
+app.get('/videos', async (req, res) => {
+    const videoPath = './videos/sample_video.mp4';
+    const stats = await fs.promises.stat(videoPath);
+
+    res.writeHead(200, {
+        'Content-Length': stats.size,
+        'Content-Type': 'video/mp4'
+    });
+    fs.createReadStream(videoPath).pipe(res);
 })
 
 app.listen(port, () => {
